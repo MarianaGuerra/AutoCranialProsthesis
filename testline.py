b525f826de85360a6778f7e7dd7c2ccb55bb0441
Mariana Guerra <marianaguerramg@gmail.com>
2018-02-18 08:10:32 -0300

    Ainda testando em testline, agora com ang entre segmento de reta entre dois pontos e uma reta horizontal

--- testline.py
+++ testline.py
@@ -14,9 +14,17 @@ points_inv = np.array(points_inv)
 
 ext_1 = np.loadtxt("ext_1_img69.txt", delimiter=' ')
 ext_1 = np.array(ext_1)
+sext_1 = []
+for i in range(0,ext_1.shape[0], 5):
+    sext_1.append(ext_1[i])
+ext_1 = np.array(sext_1)
 
 ext_2 = np.loadtxt("ext_2_img69.txt", delimiter=' ')
 ext_2 = np.array(ext_2)
+sext_2 = []
+for i in range(0,ext_2.shape[0], 5):
+    sext_2.append(ext_2[i])
+ext_2 = np.array(sext_2)
 
 points_inv_rot = points_inv.copy()
 gap_points_rot = gap_points.copy()
@@ -47,53 +55,61 @@ mean_point = [227.96358683, 254.8181934]
 # ax.set_ylim([0, 512])
 
 fig, ax = plt.subplots()
-ax.plot(ext_1[:, 1], ext_1[:, 0], 'b-')
+ax.plot(ext_1[:, 1], ext_1[:, 0], 'b.')
 ax.plot(ext_1[0,1], ext_1[0,0], 'x')
-ax.plot(ext_2[:, 1], ext_2[:, 0], 'g-')
+ax.plot(ext_2[:, 1], ext_2[:, 0], 'g.')
 ax.plot(ext_2[0,1], ext_2[0,0], 'x')
 ax.set_xlim([0, 512])
 ax.set_ylim([0, 512])
 plt.show()
 
 # testando eliminar parte de edge nos trechos ext
-ang_coef_list = []
+
+ang_list = []
 for p in range(ext_1.shape[0] - 1):
-    ang_coef = (ext_1[p + 1][0] - ext_1[p][0]) / (ext_1[p + 1][1] - ext_1[p][1])
-    ang_coef_list += [ang_coef]
-# print(str(ang_coef_list))
-ang_coef_var = np.zeros(len(ang_coef_list) - 1)
-for q in range(len(ang_coef_list) - 1):
-    ang_coef_var[q] = abs(ang_coef_list[q] - ang_coef_list[q - 1])
+    seg_x = ext_1[p + 1][1] - ext_1[p][1]
+    seg_y = ext_1[p + 1][0] - ext_1[p][0]
+    cos_ang = (seg_x*1 + seg_y*0) / (np.sqrt(seg_x**2 + seg_y**2))
+    ang_list += [np.rad2deg(np.arccos(cos_ang))]
+#print(str(ang_coef_list))
+ang_var = np.zeros(len(ang_list) - 1)
+for q in range(len(ang_list) - 1):
+    ang_var[q] = (ang_list[q] - ang_list[q - 1])
 # parametrizar lim usando a linha de base, mediana, algo assim
-ang_coef_var = np.convolve(ang_coef_var, np.ones((11,))/11, mode='valid')
-lim = 1
+ang_var = np.convolve(ang_var, np.ones((5,))/5, mode='valid')
+lim = 1.5
 # find edge first segment
-for r in range(len(ang_coef_var) - 2):
-    if ang_coef_var[r + 1] >= ang_coef_var[r] + lim:
+edge_first_seg = 0
+for r in range(len(ang_var) - 2):
+    if ang_var[r + 1] >= ang_var[r] + lim:
         edge_first_seg = r
         break
 cutted_ext_1 = ext_1[0: edge_first_seg + 2].copy()  # not inclusive
-plt.plot(range(len(ang_coef_var)), ang_coef_var[:], '.', edge_first_seg, ang_coef_var[edge_first_seg], 'x')
+plt.plot(range(len(ang_var)), ang_var[:], '.', edge_first_seg, ang_var[edge_first_seg], 'x')
 plt.legend(['ext_1', 'edge_first_seg'], loc='best')
 plt.show()
 
-ang_coef_list = []
+
+ang_list = []
 for p in range(ext_2.shape[0] - 1):
-    ang_coef = (ext_2[p + 1][0] - ext_2[p][0]) / (ext_2[p + 1][1] - ext_2[p][1])
-    ang_coef_list += [ang_coef]
+    seg_x = ext_2[p + 1][1] - ext_2[p][1]
+    seg_y = ext_2[p + 1][0] - ext_2[p][0]
+    cos_ang = (seg_x*1 + seg_y*0) / (np.sqrt(seg_x**2 + seg_y**2))
+    ang_list += [np.rad2deg(np.arccos(cos_ang))]
 # print(str(ang_coef_list))
-ang_coef_var = np.zeros(len(ang_coef_list) - 1)
-for q in range(len(ang_coef_list) - 1):
-    ang_coef_var[q] = abs(ang_coef_list[q] - ang_coef_list[q - 1])
+ang_var = np.zeros(len(ang_list) - 1)
+for q in range(len(ang_list) - 1):
+    ang_var[q] = (ang_list[q] - ang_list[q - 1])
 # parametrizar lim usando a linha de base, mediana, algo assim
-ang_coef_var = np.convolve(ang_coef_var, np.ones((11,))/11, mode='valid')
-lim = 1
-for s in range(len(ang_coef_var)-1, 0, -1):
-        if ang_coef_var[s-1] >= ang_coef_var[s] + lim:
+ang_var = np.convolve(ang_var, np.ones((5,))/5, mode='valid')
+lim = 1.5
+edge_final_seg = 0
+for s in range(len(ang_var)-1, 0, -1):
+        if ang_var[s-1] >= ang_var[s] + lim:
             edge_final_seg = s
             break
 cutted_ext_2 = ext_2[edge_final_seg + 2: len(ext_2)-1].copy()  # not inclusive
-plt.plot(range(len(ang_coef_var)), ang_coef_var[:], '.', edge_final_seg , ang_coef_var[edge_final_seg ], 'x')
+plt.plot(range(len(ang_var)), ang_var[:], '.', edge_final_seg , ang_var[edge_final_seg ], 'x')
 plt.legend(['ext_2', 'edge_final_seg'], loc='best')
 plt.show()
 
